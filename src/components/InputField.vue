fd08581b63ef05918233c3965912057db971241a
kadapanavenumadhavareddy <venumadhavareddy8@gmail.com>
2022-04-05 19:07:07 +0530

    Updated Input component with primevue component

--- src/components/InputField.vue
+++ src/components/InputField.vue
@@ -1,61 +1,29 @@
 <template>
-  <input
-    :type="inputType"
-    :value="modelValue"
-    @input="update"
-    :placeholder="placeHolder"
-    :class="inputWidth"
-    v-on:click.prevent.self
-    class="bs"
-  />
+  <span class="p-input-icon-left p-float-label">
+    <i class="pi" :class="Icon" />
+    <slot></slot>
+    <label for="username">{{ Label }}</label>
+  </span>
 </template>
 <script>
 export default {
   props: {
-    inputType: {
-      type: String,
-      required: true,
-      default: "text",
-      validator(value) {
-        return ["text", "search", "radio"].includes(value);
-      },
-    },
-    modelValue: {
+    Icon: {
       type: String,
       required: true,
     },
-    placeHolder: {
+    Label: {
       type: String,
-      default: "Type here..",
-    },
-    inputWidth: {
-      type: String,
-      default: "Max",
-      validator(value) {
-        return ["Max", "Min"].includes(value);
-      },
-    },
-  },
-  emits: ["update:modelValue"],
-  methods: {
-    update(e) {
-      this.$emit("update:modelValue", e.target.value);
+      default: "Label",
     },
   },
 };
 </script>
 <style scoped>
-input {
-  outline: none;
-  border: none;
-  caret-color: rgb(48, 46, 46);
-  border-radius: 4px;
-  height: 30px;
-  margin: 10px 0px 10px 10px;
-  width: 400px;
-  padding-left: 20px;
+span.p-input-icon-left.p-float-label {
+  margin-top: 20px;
 }
-.Min {
-  width: 185px;
+.pi {
+  margin-left: 5px;
 }
 </style>
